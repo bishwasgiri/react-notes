bd67a3b159eed3b7ced1abe619f9760f4a66398a
bishwasgiri <giribishwas4@gmail.com>
2022-07-11 07:20:34 +0545

    1.React-JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+const title = 'React';
+const welcome = {
+  greeting: 'Hey',
+  title:'React'
+}
+
+const greet = (titles)=>{
+  return titles;
+}
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello {title}</h1>
+      {/*everything in curly braces in JSX can be used for Javascript expressions */}
+      <h1>{welcome.greeting}{welcome.title}</h1>
+      <h1>{greet('Hello React')}</h1> 
+      <label htmlFor="search">Search:</label>
+      <input type="text" id="search" />
     </div>
   );
 }
