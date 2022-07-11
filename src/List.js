04e877c00a79921bf3c4adf6c2231304b56af2ee
bishwasgiri <giribishwas4@gmail.com>
2022-07-11 08:18:37 +0545

    4.Advance Component Use of Arrow Function

--- src/List.js
+++ src/List.js
@@ -1,19 +1,37 @@
+const list = [
+    {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+    },
+    {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+    }
+];
 
-function List() {
+const List = () =>{
     return(
         <div>
-            {list.map(function(item){
-                return(
-                    <div key = {item.objectID}>
-                        <span>
-                            <a href={item.url}>{item.title}</a>
-                        </span>
-                        <span>{item.author}</span>
-                        <span>{item.num_comments}</span>
-                        <span>{item.points}</span>
-                    </div>                   
-                );
-            })}
+            {/* in case return statement does not do anything but return a statment only then curly braces can be ommited */}
+            {/* return staement is also removed */}
+            {list.map(item =>(    
+                <div key = {item.objectID}>
+                    <span>
+                        <a href={item.url}>{item.title}</a>
+                    </span>
+                    <span>{item.author}</span>
+                    <span>{item.num_comments}</span>
+                    <span>{item.points}</span>
+                </div>                                  
+            ))}
         </div>
     );
 }
